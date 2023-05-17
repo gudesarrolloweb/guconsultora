087a73a1ad627739d52bc88c46ac829fb25f5d89
Gabriel Urquiza <urquizagabriel95@hotmail.com>
2023-05-17 17:38:07 -0300

    agregue javascript

---
+++ script.js
@@ -0,0 +1,109 @@
+
+/***************************Calzado de Seguridad*********************/
+/***************************Calzado de Seguridad*********************/
+/**********zapato ombu ozono*********/
+let zoonegroBtn = document.getElementById("zoonegro");
+let zoomarronBtn = document.getElementById("zoomarron");
+let zapatoombuozono = document.getElementById("zapatoombuozono");
+
+zoonegroBtn.onclick = function () {
+    zapatoombuozono.src = "../img/zapato ombu ozono negro.jpg" ;
+}
+zoomarronBtn.onclick = function () {
+    zapatoombuozono.src = "../img/zapato ombu ozono marrón.jpg";
+}
+/*******zapatilla ombu sneaker*******/
+let zosnegroBtn = document.getElementById("zosnegro");
+let zosmarronBtn = document.getElementById("zosmarron");
+let zosgrisBtn = document.getElementById("zosgris");
+let zapatillaombusneaker = document.getElementById("zapatillaombusneaker");
+
+zosnegroBtn.onclick = function () {
+    zapatillaombusneaker.src = "../img/ombu zapatilla sneaker negro.jpg" ;
+}
+zosmarronBtn.onclick = function () {
+    zapatillaombusneaker.src = "../img/ombu zapatilla sneaker marrón.jpg";
+}
+zosgrisBtn.onclick = function () {
+    zapatillaombusneaker.src = "../img/ombu zapatilla sneaker gris.jpg";
+}
+/*******zapatilla ombu kryton*******/
+let zoknegroBtn = document.getElementById("zoknegro");
+let zokazulBtn = document.getElementById("zokazul");
+let zapatillaombukrypton = document.getElementById("zapatillaombukrypton");
+
+zoknegroBtn.onclick = function () {
+    zapatillaombukrypton.src = "../img/ombu zapatilla krypton azul negro.jpg" ;
+}
+zokazulBtn.onclick = function () {
+    zapatillaombukrypton.src = "../img/ombu zapatilla krypton azul marino.jpg";
+}
+/***********botin ombu ozono*********/
+let boonegroBtn = document.getElementById("boonegro");
+let boomarronBtn = document.getElementById("boomarron");
+let booblancoBtn = document.getElementById("booblanco");
+let botinombuozono = document.getElementById("botinombuozono");
+
+boonegroBtn.onclick = function () {
+    botinombuozono.src = "../img/botin ombu ozono negro.jpg" ;
+}
+boomarronBtn.onclick = function () {
+    botinombuozono.src = "../img/botin ombu ozono marrón.jpg";
+}
+booblancoBtn.onclick = function () {
+    botinombuozono.src = "../img/botin ombu ozono blanco.jpg";
+}
+/***********bota ombu sneaker*********/
+let bosmarronBtn = document.getElementById("bosmarron");
+let bosgrisBtn = document.getElementById("bosgris");
+let botaombusneaker = document.getElementById("botaombusneaker");
+
+bosmarronBtn.onclick = function () {
+    botaombusneaker.src = "../img/ombu bota sneaker marrón.jpg";
+}
+bosgrisBtn.onclick = function () {
+    botaombusneaker.src = "../img/ombu bota sneaker gris.jpg";
+}
+/***********botin ombu krypton*********/
+let boknegroBtn = document.getElementById("boknegro");
+let bokazulBtn = document.getElementById("bokazul");
+let botinombukrypton = document.getElementById("botinombukrypton");
+
+boknegroBtn.onclick = function () {
+    botinombukrypton.src = "../img/ombu botín krypton azul negro.jpg";
+}
+bokazulBtn.onclick = function () {
+    botinombukrypton.src = "../img/ombu botín krypton azul marino.jpg";
+}
+/**************************Indumentaria*******************************/
+/**************************Indumentaria*******************************/
+/*************Pantalon Cargo*************/
+let pcbeigeBtn = document.getElementById("pcbeige");
+let pcnegroBtn = document.getElementById("pcnegro");
+let pcazulBtn = document.getElementById("pcazul");
+let pantaloncargo = document.getElementById("pantaloncargo");
+
+pcbeigeBtn.onclick = function () {
+    pantaloncargo.src = "../img/pantalon cargo beige.png" ;
+}
+pcnegroBtn.onclick = function () {
+    pantaloncargo.src = "../img/pantalon cargo negro.png";
+}
+pcazulBtn.onclick = function () {
+    pantaloncargo.src = "../img/pantalon cargo azul.png";
+}
+/*************Bombacha de campo*************/
+let bcbeigeBtn = document.getElementById("bcbeige");
+let bcnegroBtn = document.getElementById("bcnegro");
+let bcverdeBtn = document.getElementById("bcverde");
+let bombachadecampo = document.getElementById("bombachadecampo");
+
+bcbeigeBtn.onclick = function () {
+    bombachadecampo.src = "../img/bombacha de campo beige.png" ;
+}
+bcnegroBtn.onclick = function () {
+    bombachadecampo.src = "../img/bombacha de campo negro.png" ;
+}
+bcverdeBtn.onclick = function () {
+    bombachadecampo.src = "../img/bombacha de campo verde.png";
+}
